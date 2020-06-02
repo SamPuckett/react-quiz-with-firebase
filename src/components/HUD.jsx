2af14b5b7d752d16c6cba05b1e1b91bd28f75811
Samuel Ruiz <SamuelPuckett77@gmail.com>
2020-06-02 04:27:31 -0700

    Finish Game Mobile Styles
    - this commit will add styles for the mobile verion version of the game page

--- src/components/HUD.jsx
+++ src/components/HUD.jsx
@@ -5,11 +5,11 @@ import ProgressBar from './ProgressBar';
 const HUD = ({ questionNumber, score }) => {
   return (
     <div id='hud'>
-      <div className='hid-item'>
+      <div className='hud-item'>
         <p className='hud-prefix'>PROGRESS</p>
         <ProgressBar current={questionNumber} max={10} />
       </div>
-      <div className='hid-item'>
+      <div className='hud-item'>
         <p className='hud-prefix'>SCORE</p>
         <h1 className='hud-main-text'>{score}</h1>
       </div>
